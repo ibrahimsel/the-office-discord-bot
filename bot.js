5d8c2e1f7bbcceea4dc55725e66ce3e97a99f9cb
Ibrahim <63649222+ibrahimsel@users.noreply.github.com>
2021-03-25 18:13:23 +0300

    uploaded clenaer version of code

--- bot.js
+++ bot.js
@@ -7,42 +7,36 @@ client.once('ready', () => {
 
 client.on('message', async message => {
 	if (message.author.bot) return;
-
-	/*const fs = require('fs')
-	fs.readFile('scores.txt', (err, data) => {
-		if (err) throw err;
-		console.log(data.toString());
-	})
-    */
 	var dcMessage = message.content;
 	var voiceChannel = message.member.voice.channel;
 	var audio_file = "Audio Files/" + dcMessage + ".mp3"
 	
 	var current_audio_files = ["kms", "theme", "godno", "lizardking", "lemonhead",
                                 "oof", "imfine", "lemonhead", "ryan_bitch", 
-				"sexuality", "idiot", "perfektenschlag", "perfektenschlag_left",
-				"perfektenschlag_right", "britney", "bankruptcy", "kelly_slut",
-						 	    "wellwell", "turtles", "whyareyou", "calm", "procedure",
-							    "deadinside", "effectonwomen", "depression", "ignorantslut",
-							    "sun", "alcohol", "hurt", "guidance", "nooneasked", 
-							    "friendship", "ty", "beyonce", "badumts",
-							    "nobodycares", "hero", "roasted", "stutter", "identity", 
-							    "bs", "mylife", "dancing", "hentai", "boyhaveyou", "welcoming", "scranton", 
-							    "prostate", "babyback", "ohh", "sleepwithhim", "waterisfine", "chicken", 
+								"sexuality", "idiot", "perfektenschlag", "perfektenschlag_left",
+								"perfektenschlag_right", "britney", "bankruptcy", "kelly_slut",
+								"wellwell", "turtles", "whyareyou", "calm", "procedure",
+								"deadinside", "effectonwomen", "depression", "ignorantslut",
+								"sun", "alcohol", "hurt", "guidance", "nooneasked", 
+								"friendship", "ty", "beyonce", "badumts",
+								"nobodycares", "hero", "roasted", "stutter", "identity", 
+								"bs", "mylife", "dancing", "hentai", "boyhaveyou", "welcoming", "scranton", 
+								"prostate", "babyback", "ohh", "sleepwithhim", "waterisfine", "chicken", 
 								"justlostaclient", "waa", "woo", "dancinginthegym", "dammit", "itsmichael",
-							    "puke", "ayy", "no", "skeletonman"]
+								"puke", "ayy", "no", "skeletonman"]
+
+
 
 	if (current_audio_files.includes(dcMessage) && message.member.voice.channel)
 	{
 		const connection = await message.member.voice.channel.join();
 		const dispatcher = connection.play(audio_file);
+		
 		dispatcher.on('start', () => {
 			message.channel.send("playing " + dcMessage);
-			console.log(voiceChannel.members);
 		});
 
 		dispatcher.on('finish', () => {
-			// message.channel.send("finished playing");
 			voiceChannel.leave();
 		});
 
@@ -50,20 +44,39 @@ client.on('message', async message => {
 	}
 	
 	else if (current_audio_files.includes(dcMessage) && !message.member.voice.channel)
-	{
-		message.channel.send("connect to a voice channel first");
-	}
+	    message.channel.send("connect to a voice channel first");
+	
 	
 	if (dcMessage.search("İ") != -1)
-		dcMessage = dcMessage.replace(/İ/g, "I");
-	/*
-	if (dcMessage.search(/pam/i) != -1)
-		message.channel.send("Dunder Mifflin this is Pam");
-	
-	if (dcMessage.search(/dwight/i) != -1)
-		message.channel.send("Bears, beets, Battlestar Galactica");
-        */
-	
+	    dcMessage = dcMessage.replace(/İ/g, "I");
+
+    listMessage = "\nCurrent quotes:\n" + 
+							 "Michael Scott\n-------\n" + 
+							 "alcohol-bankruptcy-britney" +
+							 "\ncalm-deadinside godno" +
+							 "\nidiot ignorantslut illkillyou" +
+							 "\nkms turtles wellwell" +
+							 "\nwhyareyou hurt guidance" +
+							 "\nnooneasked beyonce" +
+							 "\nnobodycares hero roasted bs" +
+							 "\nmylife dancing welcoming chicken" +
+							 "\nitsmichael\n" + 
+							 "\nDwight Schrute\n-------\n" +
+							 "depression perfektenschlag perfektenschlag_right" +
+							 "\nperfektenschlag_left procedure identity onecrisis" +
+							 "\nohh waterisfine puke no\n" +
+							 "\nRobert California\n-------\n" +
+							 "imfine lizardking oof" +
+							 "\nsexuality\n" +
+							 "\nOthers\n-------\n" +
+							 "effectonwomen kellyslut ryanbitch" +
+							 "\nsun theme friendship ty" +
+							 "\nbadumts stutter hentai boyhaveyou" +
+							 "\nscranton prostate babyback sleepwithhim" +
+							 "\njustlostaclient woo waa dancinginthegym dammit" +
+							 "\nayy skeletonman";
+
+    listMessage = listMessage.replace(/ /g, "-")
 	switch (dcMessage)
 	{
 		case "!help":
@@ -72,37 +85,13 @@ client.on('message', async message => {
 			break;
 
 		case "!list":
-			message.channel.send("\nCurrent quotes:\n\n" + 
-							 "Michael Scott\n-------\n" + 
-							 "alcohol bankruptcy britney" +
-							 "\ncalm deadinside godno" +
-							 "\nidiot ignorantslut illkillyou" +
-							 "\nkms turtles wellwell" +
-							 "\nwhyareyou hurt guidance" +
-							 "\nnooneasked beyonce" +
-							 "\nnobodycares hero roasted bs" +
-							 "\nmylife dancing welcoming chicken" +
-							 "\nitsmichael" + 
-							 "\nDwight Schrute\n-------\n" +
-							 "depression perfektenschlag perfektenschlag_right" +
-							 "\nperfektenschlag_left procedure identity onecrisis" +
-							 "\nohh waterisfine puke no" +
-							 "\nRobert California\n-------\n" +
-							 " imfine lizardking oof" +
-							 "\n sexuality" +
-							 "\nOthers\n-------\n" +
-							 "effectonwomen kelly_slut ryan_bitch" +
-							 "\nsun theme friendship ty" +
-							 "\nbadumts stutter hentai boyhaveyou" +
-							 "\nscranton prostate babyback sleepwithhim" +
-							 "\njustlostaclient woo waa dancinginthegym dammit" +
-							 "\nayy skeletonman");
+			message.channel.send(listMessage);
 			break;
+
 		case "!author":
-			message.channel.send("\n*NAME: Ibrahim Sel\nJOB: Aspiring Artificial Intelligence Specialist*\n")
+			message.channel.send("\n*NAME: Ibrahim Sel\nJOB: Aspiring Artificial Intelligence Specialist*\n");
 			break;
-	
 	}
 });
 
-client.login("Your bot token");
+client.login("");  // insert your bot token here
